573e64ee7b274d843fe02ebeb8321ba766eccd32
dale <dale.moody@gmail.com>
2020-07-25 14:05:44 +0100

    Making RetirementService async

--- TaxCalcService/Controllers/RetirementController.cs
+++ TaxCalcService/Controllers/RetirementController.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TaxCalcService.Models;
 using TaxCalcService.Models.DTO;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TaxCalcService.Controllers
 {
@@ -23,10 +19,10 @@ namespace TaxCalcService.Controllers
 
         // GET api/Retirement/Report?salary=20000&spending=19000&dob=1981-30-05
         [HttpGet("Report")]
-        public RetirementReportDto GetReport([FromQuery]int salary, [FromQuery]int spending, [FromQuery] DateTime dob)
+        public async Task<RetirementReportDto> GetReport([FromQuery] int salary, [FromQuery] int spending,
+            [FromQuery] DateTime dob)
         {
-            return _retirement.RetirementReportFor(salary, spending, dob);
+            return await Task.Run(() => _retirement.RetirementReportFor(salary, spending, dob));
         }
-
     }
 }
