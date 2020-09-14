31889f9babc6df9693e6984d5b46066dab6bdf6a
dale <dale.moody@gmail.com>
2020-09-14 10:34:14 +0100

    Removing unused projects

--- PersonalFinanceDB/dbo/Tables/PrivatePension.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[PrivatePension]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Player_Id] INT NOT NULL, 
-    [Amount] MONEY NULL, 
-    CONSTRAINT [FK_PrivatePension_Player] FOREIGN KEY (Player_Id) REFERENCES [Player](Id)
-)
--- PersonalFinanceDB/dbo/Tables/RentalProperty.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[RentalProperty]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Player_Id] INT NOT NULL, 
-    [Current Value] MONEY NULL, 
-    [Outstanding Loan] MONEY NULL, 
-    [Rental] MONEY NULL, 
-    CONSTRAINT [FK_RentalProperty_Player] FOREIGN KEY ([Player_Id]) REFERENCES [Player](Id)
-)
